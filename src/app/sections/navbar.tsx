16a2f383de277fb4a78b621b551f00f221e2e9a0
samanthacel <samantha.michelle1711@gmail.com>
2024-11-14 14:43:43 +0700

    setup aceternity ui

---
+++ src/app/sections/navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+      <div className="navbar">
+        <h1 className="text-3xl font-bold text-black">Hello world!</h1>
+        <h2>test</h2>
+      </div>
+    );
+  }
+  
+  
